2cbd5279783ecfe570904ec0ce4ad84097866562
ysong02 <yxsong929@gmail.com>
2025-08-21 11:09:53 +0200

    attestation: create attestation library

---
+++ mari/attestation.h
@@ -0,0 +1,31 @@
+#ifndef __ATTESTATION_H
+#define __ATTESTATION_H
+
+/**
+ * @ingroup     mari
+ * @brief       attestation header file
+ *
+ * @{
+ * @file
+ * @author Yuxuan Song <yuxuan.song@inria.fr>
+ * @copyright Inria, 2025
+ * @}
+ */
+
+#include <nrf.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+//=========================== defines =========================================
+
+#define MAX_SIG_STRUCTURE   255
+
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+uint8_t mr_attestation_evidence_generation (uint64_t asn_dl, uint8_t *buffer, uint8_t *buffer_size);
+bool mr_attestation_check_version (uint8_t *buffer, uint32_t expected_version);
+
+#endif  // __ATTESTATION_H
